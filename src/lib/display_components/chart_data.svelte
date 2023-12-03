f28107ecc04922e2812b78d5e1bfcd63b00ab4a6
Tomasz Kawalec <tkawalec369@gmail.com>
2023-12-03 15:34:34 +0100

    Use Chart.js instead of LayerCake

--- src/lib/display_components/chart_data.svelte
+++ src/lib/display_components/chart_data.svelte
@@ -1,35 +1,63 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+	import type { Writable } from 'svelte/store';
+
 	import Paper, { Title, Content } from '@smui/paper';
 
-	import { Html, LayerCake, ScaledSvg } from 'layercake';
-	import Line from '$lib/chart_components/Line.svelte';
-	import AxisX from '$lib/chart_components/AxisX.html.svelte';
-	import AxisY from '$lib/chart_components/AxisY.html.svelte';
+	import Chart from 'chart.js/auto';
 
-	import { t, l, locales } from '$lib/translations';
+	import { t } from '$lib/translations';
 
-	const points = [
+	let chartCanvas: HTMLCanvasElement;
+
+	const points: Writable<Array<{ x: number; y: number }>> = writable<
+		Array<{ x: number; y: number }>
+	>([
 		{ x: 0, y: 0 },
 		{ x: 5, y: 8 },
 		{ x: 10, y: 30 },
 		{ x: 15, y: 30 },
 		{ x: 20, y: 45 }
-	];
+	]);
+
+	onMount(() => {
+		let ctx = chartCanvas.getContext('2d')!;
+		let chart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: $points.map((p) => p.x),
+				datasets: [
+					{
+						label: 'Some data',
+						data: $points.map((p) => p.y)
+					}
+				]
+			}
+		});
+		points.subscribe((value) => {
+			chart.data.labels = value.map((p) => p.x);
+			chart.data.datasets[0].data = value.map((p) => p.y);
+			chart.update();
+		});
+
+		setInterval(() => {
+			let pointsUpdated;
+			pointsUpdated = $points;
+			console.log(pointsUpdated);
+			for (let i = 0; i < pointsUpdated.length; i++) {
+				pointsUpdated[i].y = Math.random() * 50;
+			}
+			points.set(pointsUpdated);
+		}, 1000);
+	});
 </script>
 
 <Paper>
 	<Title>{$t('content.data_on_chart')}</Title>
 	<Content>
 		<div class="chart-container">
-			<LayerCake ssr={true} percentRange={true} x="x" y="y" data={points}>
-				<Html>
-					<AxisX />
-					<AxisY xTick={-4} yTick={2} ticks={8} />
-				</Html>
-				<ScaledSvg>
-					<Line stroke="#44ff44" />
-				</ScaledSvg>
-			</LayerCake>
+			<canvas bind:this={chartCanvas} id="myChart" />
 		</div>
 	</Content>
 </Paper>
